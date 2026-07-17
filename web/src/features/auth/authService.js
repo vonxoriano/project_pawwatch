3e182af3aaf97bcbe9e65f04a82ae07564ba85a4
LAB22_USER ON WS20 <L22X16W20@LabHCI.net>
2026-07-17 17:56:24 +0800

    configure web

--- web/src/features/auth/authService.js
+++ web/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth';
+const API_URL = 'https://project-pawwatch.onrender.com';
 
 const register = (fullName, email, contactNumber, password) => {
     return axios.post(`${API_URL}/register`, {
